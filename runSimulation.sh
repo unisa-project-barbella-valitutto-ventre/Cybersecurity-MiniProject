2e5ba0f2da6705abc36fb1ea2f19c6e3e7fb8354
salvatoreventre <58272078+salventre@users.noreply.github.com>
2021-02-15 16:35:39 +0100

    Update runSimulation.sh

--- runSimulation.sh
+++ runSimulation.sh
@@ -1,3 +1,3 @@
-gnome-terminal -- /bin/sh -c 'cd Cybersecurity-MiniProject/authorityServer/; pwd; python3 authorityServer.py'
-gnome-terminal -- /bin/sh -c 'cd Cybersecurity-MiniProject/analyticsServer/; pwd; python3 analyticsServer.py'
-gnome-terminal -- /bin/sh -c 'cd Cybersecurity-MiniProject/client/; pwd; python3 client.py'
+gnome-terminal -- /bin/sh -c 'cd authorityServer/; python3 authorityServer.py'
+gnome-terminal -- /bin/sh -c 'cd analyticsServer/; python3 analyticsServer.py'
+gnome-terminal -- /bin/sh -c 'cd client/; python3 client.py'
